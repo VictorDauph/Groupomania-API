8c0243926038a323c0ca8956de6bf83152d2ea39
VictorDauph <victor.dauph@gmail.com>
2022-02-01 17:39:08 +0100

    vsdsd

--- controllers/postController.js
+++ controllers/postController.js
@@ -85,11 +85,9 @@ exports.likePost= (req, res, next) =>{
     'image/jpeg': 'jpg',
     'image/png': 'png'
   };
-  const title= (req, file, callback) => {
-      const name = req.body.title.split(' ').join('_'); //suppression des espaces
-      const extension = MIME_TYPES[file.mimetype]; //définition des formats de fichiers supportés dans l'objet MIME_TYPES
-      callback(null, name + Date.now() + '.' + extension ); //formatage du nom de fichier: nom du fichier d'origine + timestamp + . + extension
-  }
+    const title= req.body.title.split(' ').join('_')+Date.now()
+
+
 
     try {
       //On récupère l'image sous forme de string depuis la requête
