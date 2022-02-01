3d0a5af1b83c0ea60251191762e850456688b24e
VictorDauph <victor.dauph@gmail.com>
2022-02-01 16:29:12 +0100

    update public id

--- controllers/postController.js
+++ controllers/postController.js
@@ -87,9 +87,7 @@ exports.likePost= (req, res, next) =>{
       const uploadResponse = await cloudinary.uploader.upload(fileStr, {
           //upload presets correspond au nom du presets à utiliser sur cloudinary, configuré au préalable directement sur cloudinary.
           upload_preset: 'Groupomania-pictures',
-          options = {
-            public_id:'gogole'
-          }
+          public_id:'gogole'
       });
       console.log(uploadResponse);
       res.json({ msg: 'image uploadée' });
